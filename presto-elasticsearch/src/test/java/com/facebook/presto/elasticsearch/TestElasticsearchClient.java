b2caa5f00893cac6c054956889fa2a890f7c221d
Alberto São Marcos <alberto.csm@gmail.com>
2016-01-13 17:36:09 +0000

    various fixes and changes on ES connector  - fix connector setup in presto (it was missing some 'configs' and was not available in runtime)  - fix mapping in column parsing  - added support for non specific index on a table (it uses all the available indices)  - fix some formating issues (https://github.com/airlift/codestyle)  - fixed case sensitive mapped table names  - fixed broken API usage (eg. presto SPI)  - added a few tests (WIP as it depends on having a Es cluster already running)

--- presto-elasticsearch/src/test/java/com/facebook/presto/elasticsearch/TestElasticsearchClient.java
+++ presto-elasticsearch/src/test/java/com/facebook/presto/elasticsearch/TestElasticsearchClient.java
@@ -1,16 +1,3 @@
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.facebook.presto.elasticsearch;
 
 import com.google.common.io.Resources;
@@ -21,14 +8,14 @@ import java.net.URI;
 import java.net.URL;
 import java.util.Map;
 
-import static com.facebook.presto.elasticsearch.ElasticsearchTestConstants.ES_SCHEMA1;
-import static com.facebook.presto.elasticsearch.ElasticsearchTestConstants.ES_TBL_1;
 import static com.facebook.presto.elasticsearch.MetadataUtil.CATALOG_CODEC;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
 public class TestElasticsearchClient
 {
-    public static final int EXPECTED_NR_OF_COLUMNS = 4;
+    private static final String ES_SCHEMA = "be";
+    private static final String ES_TBL_1 = "fancyPantsTable";
     private ElasticsearchClient client;
 
     @BeforeClass
@@ -54,9 +41,9 @@ public class TestElasticsearchClient
     public void testTable()
             throws Exception
     {
-        ElasticsearchTable table = client.getTable(ES_SCHEMA1, ES_TBL_1);
+        ElasticsearchTable table = client.getTable(ES_SCHEMA, ES_TBL_1);
         assertNotNull(table);
         assertNotNull(table.getColumns());
-        // assertEquals(table.getColumns().size(), EXPECTED_NR_OF_COLUMNS);
+        assertEquals(table.getColumns().size(), 24);
     }
 }
