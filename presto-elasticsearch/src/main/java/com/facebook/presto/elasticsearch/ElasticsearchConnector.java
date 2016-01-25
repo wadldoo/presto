a5b43d50acf09293804d211441e7ae31597c7810
Lucian Matei <lucian.matei@atige.com>
2016-01-25 21:12:54 +0200

    Convert elasticsearch connector to TableLayout API
     remove the legacy partition API

--- presto-elasticsearch/src/main/java/com/facebook/presto/elasticsearch/ElasticsearchConnector.java
+++ presto-elasticsearch/src/main/java/com/facebook/presto/elasticsearch/ElasticsearchConnector.java
@@ -1,9 +1,11 @@
 package com.facebook.presto.elasticsearch;
 
-import com.facebook.presto.spi.Connector;
-import com.facebook.presto.spi.ConnectorMetadata;
-import com.facebook.presto.spi.ConnectorRecordSetProvider;
-import com.facebook.presto.spi.ConnectorSplitManager;
+import com.facebook.presto.spi.connector.Connector;
+import com.facebook.presto.spi.connector.ConnectorMetadata;
+import com.facebook.presto.spi.connector.ConnectorRecordSetProvider;
+import com.facebook.presto.spi.connector.ConnectorSplitManager;
+import com.facebook.presto.spi.connector.ConnectorTransactionHandle;
+import com.facebook.presto.spi.transaction.IsolationLevel;
 import io.airlift.bootstrap.LifeCycleManager;
 import io.airlift.log.Logger;
 
@@ -20,22 +22,32 @@ public class ElasticsearchConnector
     private final ElasticsearchMetadata metadata;
     private final ElasticsearchSplitManager splitManager;
     private final ElasticsearchRecordSetProvider recordSetProvider;
+    private final ElasticsearchHandleResolver handleResolver;
 
     @Inject
     public ElasticsearchConnector(
             LifeCycleManager lifeCycleManager,
             ElasticsearchMetadata metadata,
             ElasticsearchSplitManager splitManager,
-            ElasticsearchRecordSetProvider recordSetProvider)
+            ElasticsearchRecordSetProvider recordSetProvider,
+            ElasticsearchHandleResolver handleResolver)
     {
         this.lifeCycleManager = requireNonNull(lifeCycleManager, "lifeCycleManager is null");
         this.metadata = requireNonNull(metadata, "metadata is null");
         this.splitManager = requireNonNull(splitManager, "splitManager is null");
         this.recordSetProvider = requireNonNull(recordSetProvider, "recordSetProvider is null");
+        this.handleResolver = requireNonNull(handleResolver, "handleResolver is null");
     }
 
     @Override
-    public ConnectorMetadata getMetadata()
+    public ConnectorTransactionHandle beginTransaction(IsolationLevel isolationLevel, boolean readOnly)
+    {
+        //TODO implement this
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ConnectorMetadata getMetadata(ConnectorTransactionHandle transactionHandle)
     {
         return metadata;
     }
--- presto-elasticsearch/src/main/java/com/facebook/presto/elasticsearch/ElasticsearchHandleResolver.java
+++ presto-elasticsearch/src/main/java/com/facebook/presto/elasticsearch/ElasticsearchHandleResolver.java
@@ -6,19 +6,22 @@ import com.facebook.presto.spi.ConnectorSplit;
 import com.facebook.presto.spi.ConnectorTableHandle;
 import com.facebook.presto.spi.ConnectorTableLayoutHandle;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
+
 public class ElasticsearchHandleResolver
         implements ConnectorHandleResolver
 {
     @Override
-    public Class<? extends ConnectorTableLayoutHandle> getTableLayoutHandleClass()
-    {
-        return ElasticsearchTableLayoutHandle.class;
-    }
-
-    @Override
     public Class<? extends ConnectorTableHandle> getTableHandleClass()
     {
         return ElasticsearchTableHandle.class;
+    }
+
+    @Override
+    public Class<? extends ConnectorTableLayoutHandle> getTableLayoutHandleClass()
+    {
+        return null;
     }
 
     @Override
@@ -31,5 +34,33 @@ public class ElasticsearchHandleResolver
     public Class<? extends ConnectorSplit> getSplitClass()
     {
         return ElasticsearchSplit.class;
+    }
+
+    static ElasticsearchTableHandle convertTableHandle(ConnectorTableHandle tableHandle)
+    {
+        requireNonNull(tableHandle, "tableHandle is null");
+        checkArgument(tableHandle instanceof ElasticsearchTableHandle, "tableHandle is not an instance of ElasticsearchTableHandle");
+        return (ElasticsearchTableHandle) tableHandle;
+    }
+
+    static ElasticsearchColumnHandle convertColumnHandle(ColumnHandle columnHandle)
+    {
+        requireNonNull(columnHandle, "columnHandle is null");
+        checkArgument(columnHandle instanceof ElasticsearchColumnHandle, "columnHandle is not an instance of ElasticsearchColumnHandle");
+        return (ElasticsearchColumnHandle) columnHandle;
+    }
+
+    static ElasticsearchSplit convertSplit(ConnectorSplit split)
+    {
+        requireNonNull(split, "split is null");
+        checkArgument(split instanceof ElasticsearchSplit, "split is not an instance of ElasticsearchSplit");
+        return (ElasticsearchSplit) split;
+    }
+
+    static ElasticsearchTableLayoutHandle convertLayout(ConnectorTableLayoutHandle layout)
+    {
+        requireNonNull(layout, "layout is null");
+        checkArgument(layout instanceof ElasticsearchTableLayoutHandle, "layout is not an instance of ElasticsearchTableLayoutHandle");
+        return (ElasticsearchTableLayoutHandle) layout;
     }
 }
