c16414d806743a2d6b3cccd7b3dd52ffa595f259
Flávio Ferreira <fferreira@bikeemotion.com>
2016-09-27 17:16:50 +0100

    Fix tests in presto elasticsearch

--- presto-elasticsearch/src/test/java/com/facebook/presto/elasticsearch/ElasticsearchTestConstants.java
+++ presto-elasticsearch/src/test/java/com/facebook/presto/elasticsearch/ElasticsearchTestConstants.java
@@ -17,7 +17,7 @@ public final class ElasticsearchTestConstants
 {
     public static final String ES_SCHEMA1 = "es_schema1";
     public static final String ES_SCHEMA2 = "es_schema2";
-    public static final String ES_TBL_1 = "movies";
+    public static final String ES_TBL_1 = "fancyPantsTable";
     public static final String CONNECTOR_ID = "TEST";
 
     private ElasticsearchTestConstants()
